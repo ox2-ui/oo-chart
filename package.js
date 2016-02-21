4dc98ccbedc53a38593076ca05bc4ce424ffd2ee
Lauricio Su <me@lauricio.com>
2016-02-21 21:07:57 +0700

    use core less package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:chart',
   summary: 'TESTING_DO_NOT_USE Chart components',
-  version: '1.3.0',
+  version: '1.4.0',
   git: ' /* Fill me in! */ '
 });
 
@@ -15,11 +15,12 @@ Package.onUse(function(api) {
   api.use([
     'templating',
     'ecmascript',
-    'underscore'
+    'underscore',
+    'less'
     ]);
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@2.5.0_3','mquandalle:jade@0.4.5', 'ox2:chartjs@1.0.2_1'
+    'mquandalle:jade@0.4.9', 'ox2:chartjs@1.0.2_1'
     ]);
   api.addFiles('lib/oo-chart.jade', C);
   api.addFiles('lib/oo-chart.js', C);
